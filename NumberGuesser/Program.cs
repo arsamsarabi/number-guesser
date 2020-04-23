66b55e5a88625ffeca148c06d1c4c5a4040237cf
Arsam Sarabi <arsamsarabi@me.com>
2020-04-23 09:12:33 +0100

    complete game!

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -22,20 +22,50 @@ namespace NumberGuesser
             string username = Console.ReadLine();
             Console.WriteLine("Hello {0}, Let's play a game!", username);
 
-            int correctNumber = 7;
-            int guess = 0;
+            string playAgain = "Y";
 
-            Console.WriteLine("Guess a number between 1 and 10");
-
-            while(guess != correctNumber)
+            while(playAgain == "Y")
             {
-                string input = Console.ReadLine();
-                guess = Int32.Parse(input);
+                Random random = new Random();
+                int correctNumber = random.Next(1, 11);
+                int guess = 0;
 
+                Console.WriteLine("Guess a number between 1 and 10");
 
+                while (guess != correctNumber)
+                {
+                    string input = Console.ReadLine();
+
+                    // Make sure its a number
+                    if (!int.TryParse(input, out guess))
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Please enter a number!");
+                        Console.ResetColor();
+                        continue;
+                    }
+
+                    guess = Int32.Parse(input);
+
+                    if (guess > correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Too high! try again.");
+                        Console.ResetColor();
+                    }
+                    else if (guess < correctNumber)
+                    {
+                        Console.ForegroundColor = ConsoleColor.DarkRed;
+                        Console.WriteLine("Too low! try again.");
+                        Console.ResetColor();
+                    }
+                }
+
+                Console.ResetColor();
+                Console.WriteLine("Correct!");
+                Console.WriteLine("Play again? (Y/N)");
+                playAgain = Console.ReadLine().ToUpper();
             }
-
-
         }
     }
 }
