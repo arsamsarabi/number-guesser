2d5efa5a18c6fc530395672675e4563d5cc0964a
Arsam Sarabi <arsamsarabi@me.com>
2020-04-23 09:38:46 +0100

    Play function

--- NumberGuesser/Program.cs
+++ NumberGuesser/Program.cs
@@ -9,41 +9,14 @@ namespace NumberGuesser
 
             PrintAppInfo();
 
-            string username = GetUsername();
+            GetUsernameAndGreet();
             
 
             string playAgain = "Y";
 
             while(playAgain == "Y")
             {
-                Random random = new Random();
-                int correctNumber = random.Next(1, 11);
-                int guess = 0;
-
-                Console.WriteLine("Guess a number between 1 and 10");
-
-                while (guess != correctNumber)
-                {
-                    string input = Console.ReadLine();
-
-                    // Make sure its a number
-                    if (!int.TryParse(input, out guess))
-                    {
-                        PrintColorMessage("Please enter a number!", ConsoleColor.DarkRed);
-                        continue;
-                    }
-
-                    guess = Int32.Parse(input);
-
-                    if (guess > correctNumber)
-                    {
-                        PrintColorMessage("Too high! try again", ConsoleColor.DarkRed);
-                    }
-                    else if (guess < correctNumber)
-                    {
-                        PrintColorMessage("Too low! try again", ConsoleColor.DarkRed);
-                    }
-                }
+                Play();
 
                 PrintColorMessage("Correct!", ConsoleColor.Green);
                 Console.WriteLine("Play again? (Y/N)");
@@ -59,24 +32,53 @@ namespace NumberGuesser
             string appAuthor = "Arsam Sarabi";
 
             string message = appName + " - v" + appVersion + " by " + appAuthor;
-            PrintColorMessage(message, ConsoleColor.DarkCyan, ConsoleColor.White);
+            PrintColorMessage(message, ConsoleColor.Cyan);
         }
 
-        static string GetUsername()
+        static void GetUsernameAndGreet()
         {
             Console.WriteLine("What is you name?");
             string username = Console.ReadLine();
             Console.WriteLine("Hello {0}, Let's play a game!", username);
-
-            return username;
         }
 
-        static void PrintColorMessage(string message, ConsoleColor color, ConsoleColor bgColor = ConsoleColor.Black)
+        static void PrintColorMessage(string message, ConsoleColor color)
         {
-            Console.BackgroundColor = bgColor;
             Console.ForegroundColor = color;
             Console.WriteLine(message);
             Console.ResetColor();
+        }
+
+        static void Play()
+        {
+            Random random = new Random();
+            int correctNumber = random.Next(1, 11);
+            int guess = 0;
+
+            Console.WriteLine("Guess a number between 1 and 10");
+
+            while (guess != correctNumber)
+            {
+                string input = Console.ReadLine();
+
+                // Make sure its a number
+                if (!int.TryParse(input, out guess))
+                {
+                    PrintColorMessage("Please enter a number!", ConsoleColor.DarkRed);
+                    continue;
+                }
+
+                guess = Int32.Parse(input);
+
+                if (guess > correctNumber)
+                {
+                    PrintColorMessage("Too high! try again", ConsoleColor.DarkRed);
+                }
+                else if (guess < correctNumber)
+                {
+                    PrintColorMessage("Too low! try again", ConsoleColor.DarkRed);
+                }
+            }
         }
 
     }
